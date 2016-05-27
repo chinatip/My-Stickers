943ff185f1d81a8fcc589a2427b24f5ce909dc58
namtan <namtan8888@gmail.com>
2016-05-27 22:52:03 +0700

    Update to gallery

--- app/src/main/java/com/example/asus/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/asus/cameraapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.asus.cameraapp;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.provider.MediaStore;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +19,7 @@ public class MainActivity extends Activity {
     Button button;
     ImageView imageView;
     static final int CAM_REQUEST = 1;
+    static int count = 0;
 
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
@@ -29,8 +33,10 @@ public class MainActivity extends Activity {
                 File file = getFile();
                 camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
                 startActivityForResult(camera_intent, CAM_REQUEST);
+                addImageGallery(file);
             }
         });
+
     }
 
     private File getFile() {
@@ -38,13 +44,23 @@ public class MainActivity extends Activity {
         if(!folder.exists()) {
             folder.mkdir();
         }
-        File image_file = new File(folder,"cam_image.jpg");
+        File image_file = new File(folder,"cam_image"+count+".jpg");
         return image_file;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String path = "sdcard/camera_app/cam_image.jpg";
+        String path = "sdcard/camera_app/cam_image"+count+".jpg";
         imageView.setImageDrawable(Drawable.createFromPath(path));
+        count++;
+    }
+
+
+
+    private void addImageGallery( File file ) {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.DATA, file.getAbsolutePath());
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
     }
 }

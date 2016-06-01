0185888815099f26f89b44f1ea40361a58cabaa8
namtan <namtan8888@gmail.com>
2016-06-01 21:28:10 +0700

    Edit InfoPopUp

--- app/src/main/java/com/example/asus/cameraapp/stickers/StickerView.java
+++ app/src/main/java/com/example/asus/cameraapp/stickers/StickerView.java
@@ -15,8 +15,10 @@ import android.view.View;
 
 import com.example.asus.cameraapp.R;
 import com.example.asus.cameraapp.stickers.state.CancleState;
+import com.example.asus.cameraapp.stickers.state.DownState;
 import com.example.asus.cameraapp.stickers.state.MotionState;
 import com.example.asus.cameraapp.stickers.state.MoveState;
+import com.example.asus.cameraapp.stickers.state.UpState;
 
 
 /**
@@ -177,67 +179,6 @@ public class StickerView extends View {
         mDrawController = show;
     }
 
-
-    private boolean isInController(float x, float y) {
-        int position = 4;
-        //while (position < 8) {
-            float rx = mPoints[position];
-            float ry = mPoints[position + 1];
-            RectF rectF = new RectF(rx - mControllerWidth / 2,
-                    ry - mControllerHeight / 2,
-                    rx + mControllerWidth / 2,
-                    ry + mControllerHeight / 2);
-            if (rectF.contains(x, y)) {
-                return true;
-            }
-         //   position += 2;
-        //}
-        return false;
-
-    }
-
-    private boolean isInDelete(float x, float y) {
-        int position = 0;
-        //while (position < 8) {
-        float rx = mPoints[position];
-        float ry = mPoints[position + 1];
-        RectF rectF = new RectF(rx - mDeleteWidth / 2,
-                ry - mDeleteHeight / 2,
-                rx + mDeleteWidth / 2,
-                ry + mDeleteHeight / 2);
-        if (rectF.contains(x, y)) {
-            return true;
-        }
-        //   position += 2;
-        //}
-        return false;
-
-    }
-    //判断点击区域是否在水平反转按钮区域内
-    private boolean isInReversalHorizontal(float x,float y){
-        int position = 2;
-        float rx = mPoints[position];
-        float ry = mPoints[position+1];
-
-        RectF rectF = new RectF(rx - mReversalHorWidth/2,ry-mReversalHorHeight/2,rx+mReversalHorWidth/2,ry+mReversalHorHeight/2);
-        if (rectF.contains(x,y))
-            return true;
-
-        return false;
-
-    }
-    //判断点击区域是否在垂直反转按钮区域内
-    private boolean isInReversalVertical(float x,float y){
-        int position = 6;
-        float rx = mPoints[position];
-        float ry = mPoints[position+1];
-
-        RectF rectF = new RectF(rx - mReversalVerWidth/2,ry - mReversalVerHeight/2,rx + mReversalVerWidth/2,ry+mReversalVerHeight/2);
-        if (rectF.contains(x,y))
-            return true;
-        return false;
-    }
-
     private boolean mInDelete = false;
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
@@ -252,47 +193,13 @@ public class StickerView extends View {
         MotionState state;
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if (isInController(x, y)) {
-                    mInController = true;
-                    mLastPointY = y;
-                    mLastPointX = x;
-                    break;
-                }
-
-                if (isInDelete(x, y)) {
-                    mInDelete = true;
-                    break;
-                }
-
-                if(isInReversalHorizontal(x,y)){
-                    mInReversalHorizontal = true;
-                    break;
-                }
-
-                if(isInReversalVertical(x,y)){
-                    mInReversalVertical = true;
-                    break;
-                }
-
-                if (mContentRect.contains(x, y)) {
-                    mLastPointY = y;
-                    mLastPointX = x;
-                    mInMove = true;
-                }
+                state = new DownState();
+                state.dispatchTouchEvent(event, this);
                 break;
             case MotionEvent.ACTION_UP:
-                if (isInDelete(x, y) && mInDelete) {
-                    doDeleteSticker();
-                    break;
-                }
-                if(isInReversalHorizontal(x,y) && mInReversalHorizontal){
-                    doReversalHorizontal();
-                    break;
-                }
-                if (isInReversalVertical(x,y) && mInReversalVertical){
-                    doReversalVertical();
-                    break;
-                }
+                state = new UpState();
+                state.dispatchTouchEvent(event, this);
+                break;
             case MotionEvent.ACTION_CANCEL:
                 state = new CancleState();
                 state.dispatchTouchEvent(event, this);
@@ -306,66 +213,6 @@ public class StickerView extends View {
 
         }
         return true;
-    }
-
-    public void doDeleteSticker() {
-        setWaterMark(null);
-        if (mOnStickerDeleteListener != null) {
-            mOnStickerDeleteListener.onDelete();
-        }
-    }
-
-    //图片水平反转
-    public void doReversalHorizontal(){
-        float[] floats = new float[] { -1f, 0f, 0f, 0f, 1f, 0f, 0f, 0f, 1f };
-        Matrix tmpMatrix = new Matrix();
-        tmpMatrix.setValues(floats);
-        mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(),
-                mBitmap.getHeight(), tmpMatrix, true);
-        invalidate();
-        mInReversalHorizontal = false;
-    }
-    //图片垂直反转
-    public void doReversalVertical(){
-        float[] floats = new float[] { 1f, 0f, 0f, 0f, -1f, 0f, 0f, 0f, 1f };
-        Matrix tmpMatrix = new Matrix();
-        tmpMatrix.setValues(floats);
-        mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(),
-                mBitmap.getHeight(), tmpMatrix, true);
-        invalidate();
-        mInReversalVertical = false;
-    }
-
-
-    private boolean canStickerMove(float cx, float cy) {
-        float px = cx + mPoints[8];
-        float py = cy + mPoints[9];
-        if (mViewRect.contains(px, py)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-    private float calculateLength(float x, float y) {
-        float ex = x - mPoints[8];
-        float ey = y - mPoints[9];
-        return (float)Math.sqrt(ex * ex + ey * ey);
-    }
-
-
-    private float rotation(MotionEvent event) {
-        float  originDegree = calculateDegree(mLastPointX, mLastPointY);
-        float nowDegree = calculateDegree(event.getX(), event.getY());
-        return nowDegree - originDegree;
-    }
-
-    private float calculateDegree(float x, float y) {
-        double delta_x = x - mPoints[8];
-        double delta_y = y - mPoints[9];
-        double radians = Math.atan2(delta_y, delta_x);
-        return (float) Math.toDegrees(radians);
     }
 
     public interface OnStickerDeleteListener {
@@ -639,4 +486,5 @@ public class StickerView extends View {
     public void setmInDelete(boolean mInDelete) {
         this.mInDelete = mInDelete;
     }
+
 }
--- app/src/main/java/com/example/asus/cameraapp/stickers/state/UpState.java
+++ app/src/main/java/com/example/asus/cameraapp/stickers/state/UpState.java
@@ -1,8 +1,11 @@
 package com.example.asus.cameraapp.stickers.state;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.support.annotation.NonNull;
 import android.view.MotionEvent;
 
+import com.example.asus.cameraapp.stickers.DisplayUtil;
 import com.example.asus.cameraapp.stickers.StickerView;
 
 /**
@@ -14,7 +17,14 @@ public class UpState implements MotionState {
     private StickerView stickerView;
     private boolean mInDelete;
     private boolean mInReversalHorizontal,mInReversalVertical;
+    private OnStickerDeleteListener mOnStickerDeleteListener;
+    private Bitmap mBitmap;
+    private Matrix mMatrix;
     float x,y;
+    private float[] mOriginPoints;
+    private float[] mPoints;
+    private RectF mOriginContentRect;
+    private RectF mContentRect;
 
     public void init() {
         x = event.getX();
@@ -22,6 +32,12 @@ public class UpState implements MotionState {
         mInDelete = stickerView.ismInDelete();
         mInReversalHorizontal = stickerView.ismInReversalHorizontal();
         mInReversalVertical = stickerView.ismInReversalVertical();
+        mBitmap = stickerView.getmBitmap();
+        mMatrix = stickerView.getmMatrix();
+        mOriginPoints = stickerView.getmOriginPoints();
+        mPoints = stickerView.getmPoints();
+        mOriginContentRect = stickerView.getmOriginContentRect();
+        mContentRect = stickerView.getmContentRect();
     }
     @Override
     public void dispatchTouchEvent(MotionEvent event, StickerView stickerView) {
@@ -30,17 +46,99 @@ public class UpState implements MotionState {
         checker = new Checker(stickerView);
         init();
         if (checker.isInDelete(x, y) && mInDelete) {
-            stickerView.doDeleteSticker();
+            doDeleteSticker();
             return;
         }
         if(checker.isInReversalHorizontal(x, y) && mInReversalHorizontal){
-            stickerView.doReversalHorizontal();
+            doReversalHorizontal();
             return;
         }
         if (checker.isInReversalVertical(x, y) && mInReversalVertical){
-            stickerView.doReversalVertical();
+            doReversalVertical();
             return;
         }
+    }
+
+
+    private void doDeleteSticker() {
+        setWaterMark(null);
+        if (mOnStickerDeleteListener != null) {
+            mOnStickerDeleteListener.onDelete();
+        }
+    }
+
+    //图片水平反转
+    private void doReversalHorizontal(){
+        float[] floats = new float[] { -1f, 0f, 0f, 0f, 1f, 0f, 0f, 0f, 1f };
+        Matrix tmpMatrix = new Matrix();
+        tmpMatrix.setValues(floats);
+        mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(),
+                mBitmap.getHeight(), tmpMatrix, true);
+        stickerView.setmBitmap(mBitmap);
+        stickerView.invalidate();
+        stickerView.setmInReversalHorizontal(false);
+    }
+    //图片垂直反转
+    private void doReversalVertical(){
+        float[] floats = new float[] { 1f, 0f, 0f, 0f, -1f, 0f, 0f, 0f, 1f };
+        Matrix tmpMatrix = new Matrix();
+        tmpMatrix.setValues(floats);
+        mBitmap = Bitmap.createBitmap(mBitmap, 0, 0, mBitmap.getWidth(),
+                mBitmap.getHeight(), tmpMatrix, true);
+        stickerView.setmBitmap(mBitmap);
+        stickerView.invalidate();
+        stickerView.setmInReversalVertical(false);
+    }
+
+    public interface OnStickerDeleteListener {
+        public void onDelete();
+    }
+
+    public void setOnStickerDeleteListener(OnStickerDeleteListener listener) {
+        stickerView.setOnStickerDeleteListener((StickerView.OnStickerDeleteListener) listener);
+    }
+
+    public void setWaterMark(@NonNull Bitmap bitmap) {
+        mBitmap = bitmap;
+        stickerView.setmStickerScaleSize(1.0f);
+
+
+        stickerView.setFocusable(true);
+        try {
+
+
+            float px = mBitmap.getWidth();
+            float py = mBitmap.getHeight();
+
+
+            //mOriginPoints = new float[]{px, py, px + bitmap.getWidth(), py, bitmap.getWidth() + px, bitmap.getHeight() + py, px, py + bitmap.getHeight()};
+//            mOriginPoints = new float[]{0, 0, px, 0, px, py, 0, py, px / 2, py / 2};
+//            stickerView.setmOriginPoints(mOriginPoints);
+//            mOriginContentRect = new RectF(0, 0, px, py);
+//            stickerView.setmOriginContentRect(mOriginContentRect);
+//            mPoints = new float[10];
+//            stickerView.setmPoints(mPoints);
+//            mContentRect = new RectF();
+//            stickerView.setmContentRect(mContentRect);
+            mMatrix = new Matrix();
+//            stickerView.setmMatrix(mMatrix);
+
+            stickerView.setmOriginPoints(new float[]{0, 0, px, 0, px, py, 0, py, px / 2, py / 2});
+            stickerView.setmOriginContentRect(new RectF(0, 0, px, py));
+            stickerView.setmPoints(new float[10]);
+            stickerView.setmContentRect(new RectF());
+            stickerView.setmMatrix(new Matrix());
+
+            float transtLeft = ((float) DisplayUtil.getDisplayWidthPixels(stickerView.getContext()) - mBitmap.getWidth()) / 2;
+            float transtTop = ((float)DisplayUtil.getDisplayWidthPixels(stickerView.getContext()) - mBitmap.getHeight()) / 2;
+
+            mMatrix.postTranslate(transtLeft, transtTop);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        stickerView.postInvalidate();
+
     }
 
 }
